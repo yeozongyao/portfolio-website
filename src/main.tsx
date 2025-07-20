3f6675dc94479f9cdbfd31d5a59aae1949d71c55
Yeo Zong Yao <yeozongyao@gmail.com>
2025-07-20 01:49:04 -0400

    feat: implement scroll down button

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './pages/About.tsx';
+
+import Home from './pages/LandingPage.tsx';
+import About from './pages/About.tsx';
 import AppLayout from './components/layout/Layout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppLayout>
-      <App />
+      <Home />
+      <section
+        id="about-section"
+        className="min-h-screen flex items-center justify-center bg-primary text-white"
+        style={{ scrollSnapAlign: 'start' }}
+      >
+        <About />
+      </section>
     </AppLayout>
   </StrictMode>
 );
