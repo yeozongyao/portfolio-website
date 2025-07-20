5bdf4d3ffa3ede26fdb22e7eb3eb068ba4f0e927
Yeo Zong Yao <yeozongyao@gmail.com>
2025-07-19 23:56:01 -0400

    feat: reorg files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './About.tsx';
+import App from './pages/About.tsx';
 import AppLayout from './components/layout/Layout.tsx';
 
 createRoot(document.getElementById('root')!).render(
