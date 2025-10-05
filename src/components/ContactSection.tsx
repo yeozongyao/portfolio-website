f002c330b22466c7dc7ff8f1c4cc0eced481d2cf
Yeo Zong Yao <yeozongyao@gmail.com>
2025-10-05 23:14:21 +0800

    debugging

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -29,6 +29,10 @@ export default function ContactSection() {
           console.log('Template ID:', import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
           console.log('Service ID:', import.meta.env.VITE_EMAILJS_SERVICE_ID);
           console.log('Public Key:', import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
+          console.log(JSON.stringify(import.meta.env.VITE_EMAILJS_TEMPLATE_ID));
+          console.log(JSON.stringify(import.meta.env.VITE_EMAILJS_SERVICE_ID));
+          console.log(JSON.stringify(import.meta.env.VITE_EMAILJS_PUBLIC_KEY));
           setStatusMsg('❌ Oops—something went wrong.');
         }
       );
